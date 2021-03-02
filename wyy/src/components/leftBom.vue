b7b30eff61be5d883544739c73cfb9ef143e3c4d
zzc <1036810345@qq.com>
2021-03-02 17:29:32 +0800

    日夜切换

--- wyy/src/components/leftBom.vue
+++ wyy/src/components/leftBom.vue
@@ -1,13 +1,39 @@
 <template>
     <section class="leftBomB">
         <ul class="leftBom">
-            <li><span class="iconfont icon-yejianmoshi"></span>夜间模式</li>
+            <li @click="cutDayNgint"><span class="dayNgintText iconfont icon-yejianmoshi">{{dayNgintText}}</span></li>
             <li><span class="iconfont icon-shezhi"></span>设置</li>
             <li><span class="iconfont icon-power"></span>退出</li>
         </ul>
     </section>
 </template>
 
+<script>
+export default {
+    name:'',
+    data(){
+        return{
+            dayNgintText:'夜间模式'
+        }
+    },
+    created(){
+      
+    },
+    methods:{
+        cutDayNgint(){
+            if(this.dayNgintText === '夜间模式'){
+                this.dayNgintText = '日间模式'
+            }else{
+                this.dayNgintText = '夜间模式'
+
+
+                window.app[0].style('background','#ccc')
+            }
+        
+        }
+    }
+}
+</script>
 <style scoped lang="scss">
    .leftBom{
         width: 6rem;
